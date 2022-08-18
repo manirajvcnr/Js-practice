1da5b40ce3a29f575532f8c877c1688a43e614c6
manirajvcnr <cnrakashcnr@gmail.com>
2022-08-18 16:07:02 +0530

    add-file.

---
+++ array/array iteration for loop/script.js
@@ -0,0 +1,10 @@
+// Array iteration
+let number=['0','1','2','3','4','5'];
+
+console.log(number);
+
+console.log('The array length is '+number.length);
+
+for(i=0;i<=number.length;i++){
+    console.log(i);
+}
